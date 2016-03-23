4e94d7f1825bf6752c1e63a1255a3750df8cea9c
SirWojtek <momatoku@gmail.com>
2016-03-23 13:58:35 +0100

    Refactored ship.go

--- objects/ship.go
+++ objects/ship.go
@@ -1,23 +1,15 @@
 package objects
 
 type Ship struct {
-	pos Rect
-	hp  Health
+	Rect
+	Health
 }
 
 const shipHealth = 100
 
 func NewShip(position Rect) Ship {
 	return Ship{
-		pos: position,
-		hp:  shipHealth,
+		Rect:   position,
+		Health: shipHealth,
 	}
-}
-
-func (ship *Ship) MoveBy(x, y float32) {
-	ship.pos.MoveBy(x, y)
-}
-
-func (ship *Ship) GetDamage(dmg Health) {
-	ship.hp.GetDamage(dmg)
 }
