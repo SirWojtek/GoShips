e9ce942f78c893da38c07901e251e2ef362a2363
SirWojtek <momatoku@gmail.com>
2016-04-03 15:20:51 +0200

    Add scene creating

--- objects/scene.go
+++ objects/scene.go
@@ -2,15 +2,24 @@ package objects
 
 type Scene struct {
 	*Object
+	ships []*Ship
 }
 
 func NewScene() Scene {
+	leftShip := NewShip("LeftShip", Rect{0, 30, 10, 10})
+	rightShip := NewShip("RightShip", Rect{100, 30, 10, 10})
+
 	scene := Scene{
 		Object: NewObject("Scene", Rect{0, 0, 0, 0}),
+		ships:  []*Ship{leftShip, rightShip},
 	}
 
-	scene.AddChild(NewShip("LeftShip", Rect{0, 30, 10, 10}))
-	scene.AddChild(NewShip("RightShip", Rect{100, 30, 10, 10}))
+	scene.AddChild(leftShip)
+	scene.AddChild(rightShip)
 
 	return scene
+}
+
+func (obj *Scene) MoveBy(x, y float32) {
+	panic("Scene can not be moved")
 }
