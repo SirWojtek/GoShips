632303ed68b197d0ed279290153e4b1ba36e384d
Mateusz Okulewicz <mateusz.okulewicz@gigaset.com>
2016-05-10 15:50:53 +0200

    Improved thread syncing

--- game/threads.go
+++ game/threads.go
@@ -3,6 +3,7 @@ package game
 import (
 	"github.com/SirWojtek/GoShips/controller"
 	"github.com/SirWojtek/GoShips/objects"
+	"github.com/SirWojtek/GoShips/utilities"
 	"github.com/SirWojtek/GoShips/view"
 	"sync"
 	"time"
@@ -36,15 +37,17 @@ func (t *Threads) paintLoop(
 			preController.Tick()
 		}
 
+		utilities.Log.Println("Before view loop")
 		vc.ViewLoop()
+		utilities.Log.Println("After view loop")
 
 		for _, postController := range postPaintControllers {
 			postController.Tick()
 		}
 
 		// TODO: dynamically compute sleep period (keep const FPS)
-		time.Sleep(1 / FPS * time.Second)
 		t.painted.Broadcast()
+		time.Sleep(1 / FPS * time.Second)
 	}
 }
 
