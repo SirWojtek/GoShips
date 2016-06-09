5ffcd73e25251de72be8b58ac0cab52411cd48f4
SirWojtek <momatoku@gmail.com>
2016-06-09 21:19:15 +0200

    Ship shoot period

--- objects/ship.go
+++ objects/ship.go
@@ -1,27 +1,48 @@
 package objects
 
-import "github.com/SirWojtek/GoShips/utilities"
+import (
+	"github.com/SirWojtek/GoShips/utilities"
+	"time"
+)
 
 type Ship struct {
 	*Object
 	Health
 	IsTurnedRight bool
+	canShoot      bool
+	shootTimer    <-chan time.Time
 }
 
 const shipHealth = 100
 const ShipMovementStep = 1.2
+const shipShootPeriod = 500 // ms
+
+func newShootTimer() <-chan time.Time {
+	return time.After(shipShootPeriod * time.Millisecond)
+}
 
 func NewShip(name string, position Rect, color Color, sceneBounds Rect, isTurnedRight bool) *Ship {
 	return &Ship{
 		Object:        NewObject(name, position, color, sceneBounds),
 		Health:        shipHealth,
 		IsTurnedRight: isTurnedRight,
+		canShoot:      true,
+		shootTimer:    newShootTimer(),
 	}
 }
 
 func (ship *Ship) Shoot() {
-	x, y := ship.getMissileCoords()
-	ship.AddChild(NewMissile(x, y, ship.sceneBounds))
+	select {
+	case <-ship.shootTimer:
+		ship.canShoot = true
+	default:
+		if ship.canShoot {
+			x, y := ship.getMissileCoords()
+			ship.AddChild(NewMissile(x, y, ship.sceneBounds))
+			ship.canShoot = false
+			ship.shootTimer = newShootTimer()
+		}
+	}
 }
 
 func (ship *Ship) getMissileCoords() (float32, float32) {
